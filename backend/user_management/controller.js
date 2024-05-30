1f31094eab0fc46140ae4ddc35ec4b4f85d0975b
Sarghe-Andrei-Vlad <sargheandreivlad@gmail.com>
2024-05-30 11:27:50 +0300

    Backend
    Register login 1.0

---
+++ backend/user_management/controller.js
@@ -0,0 +1,147 @@
+const http = require('http');
+const url = require('url');
+const model = require('./model.js');
+
+const jsonType = { "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS", "Access-Control-Allow-Credentials": true, "Access-Control-Allow-Headers": "authorization,content-type", "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" };
+const textType = { "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS", "Access-Control-Allow-Credentials": true, "Access-Control-Allow-Headers": "authorization,content-type", "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain" };
+const noType = { "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS", "Access-Control-Allow-Credentials": true, "Access-Control-Allow-Headers": "authorization,content-type", "Access-Control-Allow-Origin": "*" };
+
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
+function sendResponse(response, statusCode, message, headers = {}) {
+    response.writeHead(statusCode, { ...headers });
+    response.write(message);
+    response.end();
+}
+
+function send500Response(response) {
+    sendResponse(response, 500, "Error 500: Internal server error", textType);
+}
+
+function send404Response(response) {
+    sendResponse(response, 404, "Error 404: Page not found", textType);
+}
+
+function send200Response(response) {
+    sendResponse(response, 200, "Done", textType);
+}
+
+function send401Response(response) {
+    sendResponse(response, 401, "Error 401: Unauthorized", textType);
+}
+
+function send403Response(response) {
+    sendResponse(response, 403, "Error 403: Send valid query params", textType);
+}
+
+function createToken(user_id) {
+    var payload = {
+        user_id: user_id,
+    };
+    var privateKEY = fs.readFileSync('./private.key', 'utf8');
+    var publicKEY = fs.readFileSync('./public.key', 'utf8');
+    var i = 'UPNP';                   // Issuer 
+    var s = 'some@user.com';          // Subject 
+    var a = 'http://localhost';       // Audience
+    var signOptions = {
+        issuer: i,
+        subject: s,
+        audience: a,
+        expiresIn: "12h",
+        algorithm: "RS256"
+    };
+    var token = jwt.sign(payload, privateKEY, signOptions);
+    console.log("Token - " + token);
+    return token;
+}
+
+function onRequest(request, response) {
+
+    if (request.method == 'POST' && request.url == '/login') {
+        var buff = '';
+        var body = '';
+        request.on('data', chunk => {
+            buff += chunk;
+        });
+        request.on('end', () => {
+            body = JSON.parse(buff);
+            console.log("U:" + body['username']);
+            console.log("P" + body['password']);
+            if (body['username'] && body['password'])
+                model.login(body['username'], body['password']).then(function (resp) {
+                    console.log(resp);
+                    if (!resp) {
+                        send403Response(response);
+                    }
+                    else {
+                        model.getId(body['username']).then(function (user_id) {
+                            let json = { "token": createToken(user_id) };
+                            response.writeHead(200, jsonType);
+                            response.write(JSON.stringify(json));
+                            response.end();
+                        }).catch((err) => setImmediate(() => { send500Response(response); console.log(err); }));
+                    }
+                }).catch((err) => setImmediate(() => { send500Response(response); console.log(err); }));
+            else {
+                send401Response(response);
+            }
+        });
+    }
+    else if (request.method == 'POST' && request.url == '/register') {
+        var buff = '';
+        var body = '';
+        request.on('data', chunk => {
+            buff += chunk;
+        })
+        request.on('end', () => {
+            console.log("Register-" + buff);
+            body = JSON.parse(buff);
+            model.register(body['username'], body['password'], body['email']).then(function (message) {
+                let json = { "status": message };
+                console.log(json);
+                response.writeHead(200, jsonType);
+                response.write(JSON.stringify(json));
+                response.end();
+            }).catch((err) => setImmediate(() => { send500Response(response); console.log(err); }));
+        })
+    }
+    else if (request.method == "GET" && request.url.indexOf('/getId') == 0) {
+        var queryData = url.parse(request.url, true).query;
+        let username = queryData.username;
+        if (username && username != '')
+            model.getId(username).then(function (id) {
+                let json = { "id": id };
+                console.log(json);
+                response.writeHead(200, jsonType);
+                response.write(JSON.stringify(json));
+                response.end();
+            }).catch((err) => setImmediate(() => { send500Response(response); console.log(err); }));
+        else send403Response(response);
+    }
+    else if (request.method == "GET" && request.url.indexOf('/getUsername') == 0) {
+        var queryData = url.parse(request.url, true).query;
+        let userId = queryData.userId;
+        if (userId && userId != '')
+            model.getUsername(userId).then(function (username) {
+                let json = { "username": username };
+                console.log(json);
+                response.writeHead(200, jsonType);
+                response.write(JSON.stringify(json));
+                response.end();
+            }).catch((err) => setImmediate(() => { send500Response(response); console.log(err); }));
+        else send403Response(response);
+    }
+    else if (request.method == 'OPTIONS') {
+        console.log("Options " + request.url)
+        response.writeHead(200, noType);
+        response.end();
+    }
+    else {
+        console.log(request.method + " " + request.url);
+        send404Response(response);
+    }
+}
+
+http.createServer(onRequest).listen(8001);
+console.log("Service is running ");
---
+++ backend/user_management/model.js
@@ -0,0 +1,187 @@
+const bcrypt = require('bcrypt');
+
+hashPassword = function (password) {
+    let hash = bcrypt.hashSync(password, 10);
+    return hash;
+}
+
+function checkUsername(name) {
+    return new Promise(function (resolve, reject) {
+        var mysql = require('mysql');
+
+        var con = mysql.createConnection({ host: 'localhost', user: 'root', password: 'root', database: 'fosa_database' });
+        con.connect(function (err) {
+            if (err) return reject(err);
+            console.log("Connected!");
+        });
+
+        con.query('select count (*) as c from users where username=?', [name], function (err, rows, fields) {
+            if (err) return reject(err);
+            con.end();
+            console.log("checkUsername" + rows[0].c);
+            if (rows[0].c == 0) resolve(false);
+            else resolve(true);
+        });
+    });
+
+}
+
+function checkEmail(email) {
+    return new Promise(function (resolve, reject) {
+        var mysql = require('mysql');
+
+        var con = mysql.createConnection({ host: 'localhost', user: 'root', password: 'root', database: 'fosa_database' });
+        con.connect(function (err) {
+            if (err) return reject(err);
+            console.log("Connected!");
+        });
+
+        con.query('select count (*) as c from users where email=?', [email], function (err, rows, fields) {
+            if (err) return reject(err);
+            con.end();
+            console.log("check email:" + rows[0].c);
+            if (rows[0].c == 0) resolve(false);
+            else resolve(true);
+        });
+    });
+
+}
+
+function addUser(name, email, password) {
+    return new Promise(function (resolve, reject) {
+        var mysql = require('mysql');
+
+        var con = mysql.createConnection({ host: 'localhost', user: 'root', password: 'root', database: 'fosa_database' });
+        con.connect(function (err) {
+            if (err) return reject(err);
+            console.log("Connected!");
+        });
+        let hash = hashPassword(password);
+
+        con.query('insert into users(username,email,password) values (?,?,?)', [name, email, hash], function (err, rows, fields) {
+
+            if (err) return reject(err);
+            con.end();
+            resolve(true);
+
+        })
+
+    })
+}
+
+module.exports.checkStatus = function (username) {
+    return new Promise(function (resolve, reject) {
+        var mysql = require('mysql');
+
+        var con = mysql.createConnection({ host: 'localhost', user: 'root', password: 'root', database: 'fosa_database' });
+
+        con.connect(function (err) {
+            if (err) return reject(err);
+            console.log("Connected!");
+        });
+
+        con.query('select status from users where username=?', [username], function (err, rows, fields) {
+            if (err) return reject(err);
+            con.end();
+            if (rows[0].status == 1) resolve(true);
+            else resolve(false);
+        })
+
+    })
+}
+
+
+module.exports.login = function (user_name, password) {
+    return new Promise(function (resolve, reject) {
+        var mysql = require('mysql');
+
+        var con = mysql.createConnection({ host: 'localhost', user: 'root', password: 'root', database: 'fosa_database' });
+
+        con.connect(function (err) {
+            if (err) return reject(err);
+            console.log("Connected!");
+        });
+        console.log("username:" + user_name);
+        con.query('select password from users where username=? and status=1', [user_name], function (err, rows, fields) {
+            if (err) return reject(false);
+            con.end();
+            if (rows[0] == null || !rows[0]) resolve(false);
+            else {
+                let hash = rows[0].password;
+                console.log("password:" + hash);
+                if (hash)
+                    resolve(bcrypt.compareSync(password, hash));
+                else reject(false);
+            }
+
+        });
+
+    })
+}
+
+module.exports.getId = function (username) {
+    return new Promise(function (resolve, reject) {
+        var mysql = require('mysql');
+        console.log("get id:" + username);
+        var con = mysql.createConnection({ host: 'localhost', user: 'root', password: 'root', database: 'fosa_database' });
+
+        con.connect(function (err) {
+            if (err) return reject(err);
+            console.log("Connected!");
+        });
+
+        con.query('select userId from users where username=? or email=?', [username, username], function (err, rows, fields) {
+            if (err) return reject(err);
+            con.end();
+            if (rows[0])
+                resolve(rows[0].userId);
+            else reject(err);
+        });
+
+    })
+}
+
+module.exports.getUsername = function (userID) {
+    return new Promise(function (resolve, reject) {
+        var mysql = require('mysql');
+        console.log("get USERNAME for:" + userID);
+        var con = mysql.createConnection({ host: 'localhost', user: 'root', password: 'root', database: 'fosa_database' });
+
+        con.connect(function (err) {
+            if (err) return reject(err);
+            console.log("Connected!");
+        });
+
+        con.query('select username from users where userId=?', [userID], function (err, rows, fields) {
+            if (err) return reject(err);
+            con.end();
+
+            resolve(rows[0].username);
+
+        });
+
+    })
+}
+
+module.exports.register = function (name, pass, email) {
+    return new Promise(function (resolve, reject) {
+        console.log("register " + name + " " + email + " " + pass);
+
+        checkEmail(email).then(function (bool) {
+            if (bool) reject('account already exists');
+            else {
+
+                checkUsername(name).then(function (bool1) {
+                    if (bool1) reject('username already taken');
+                    else {
+                        addUser(name, email, pass).then(function (bool2) {
+                            if (bool2) {
+                                resolve('user registered succesfully');
+                            }
+                        }).catch((err) => setImmediate(() => { console.log(err); reject(err); }));
+                    }
+                }).catch((err) => setImmediate(() => { console.log(err); reject(err); }));
+            }
+        });
+    })
+}
